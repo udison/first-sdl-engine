3a41a95797f5de95457e03425d102a95484b28d8
Roque Santos <roque-100@hotmail.com>
2024-10-26 02:23:27 -0300

    refactor on engine main

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -1,5 +1,8 @@
-/*This source code copyrighted by Lazy Foo' Productions 2004-2024
-and may not be redistributed without written permission.*/
+/*
+ * First attempt at a c++ game engine using SDL2.
+ * Following this resource:
+ * https://lazyfoo.net/tutorials/SDL/
+ */
 
 //Using SDL and standard IO
 #include <SDL.h>
@@ -9,35 +12,61 @@ and may not be redistributed without written permission.*/
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char* args[])
+// The app window
+SDL_Window* g_Window = NULL;
+
+// Surface contained by the window
+SDL_Surface* g_Surface = NULL;
+
+// Image to render on screen
+SDL_Surface* g_HelloWorld = NULL;
+
+bool init()
 {
-    // The app window
-    SDL_Window *window = NULL;
-
-    // Surface contained by the window
-    SDL_Surface *surface = NULL;
-
     // Initializing SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        printf("SDL_Init error: %s\n", SDL_GetError());
-        return 1;
+        printf("SDL initialization error: %s\n", SDL_GetError());
+        return false;
     }
 
-    window = SDL_CreateWindow("Engine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == NULL)
+    // Creating a window
+    g_Window = SDL_CreateWindow("Engine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (g_Window == NULL)
     {
-        printf("SDL_CreateWindow error: %s\n", SDL_GetError());
-        return 1;
+        printf("SDL Window creation error: %s\n", SDL_GetError());
+        return false;
     }
 
-    surface = SDL_GetWindowSurface(window); // gets the surface of the window
-    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF)); // paints the window white
-    SDL_UpdateWindowSurface(window); // update its surface in order to show thing that were drawn
+    g_Surface = SDL_GetWindowSurface(g_Window); // gets the surface of the window
+
+    return true;
+}
+
+bool loadMedia()
+{
+    SDL_FillRect(g_Surface, NULL, SDL_MapRGB(g_Surface->format, 0xFF, 0xFF, 0xFF)); // paints the window white
+    SDL_UpdateWindowSurface(g_Window); // update its surface in order to show thing that were drawn
     SDL_Event e; bool quit = false; while (!quit) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } } // forces the window to stay open
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    return true;
+}
 
+void close()
+{
+    SDL_DestroyWindow(g_Window);
+    SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
+    if (!init())
+    {
+        return 1;
+    }
+    
+    loadMedia();
+    
+    close();
     return 0;
 }
